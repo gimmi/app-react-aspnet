f318ae74606434734bcaf74071629a0355426660
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-03-14 20:51:46 +0100

    Using raw ace-builds

--- client/src/EditorComponent.js
+++ client/src/EditorComponent.js
@@ -1,10 +1,37 @@
 ﻿import React from 'react';
-import AceEditor from 'react-ace';
+
+import ace from 'ace-builds/src-noconflict/ace';
 import 'ace-builds/src-noconflict/mode-json';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 export default class EditorComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.divRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const divEl = this.divRef.current;
+        this.aceEditor = ace.edit(divEl, {
+            mode: 'ace/mode/json',
+            theme: 'ace/theme/monokai',
+            showPrintMargin: false,
+            useWorker: false,
+            maxLines: 50,
+            minLines: 50
+        });
+    }
+
+    componentWillUnmount() {
+        this.aceEditor.destroy();
+    }
+
     render() {
-        return <AceEditor mode="json" theme="monokai" setOptions={{useWorker: false}} />
+        return (
+            <>
+                <h1>This is the Editor page</h1>
+                <div ref={this.divRef} />
+            </>
+        )
     }
 }
