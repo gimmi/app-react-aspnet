7c7866d9e76f6a7e91e7ab51260f2c83e138fd2c
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-03-14 20:23:10 +0100

    Updated on MainComponent

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -10,6 +10,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 import { ContextInfoComponent } from './ContextInfoComponent';
 import EditorComponent from './EditorComponent';
@@ -38,20 +40,25 @@ export function MainComponent() {
     const classes = useStyles();
 
     return (
-        <Router>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6">
-                        <Link color="inherit" component={RouterLink} className={classes.navLink} to="/">Home</Link>
-                        <Link color="inherit" component={RouterLink} className={classes.navLink} to="/editor/">Editor</Link>
-                        <Link color="inherit" component={RouterLink} className={classes.navLink} to="/users/">Users</Link>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+        <>
+            <CssBaseline />
+            <Router>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6">
+                            <Link color="inherit" component={RouterLink} className={classes.navLink} to="/">Home</Link>
+                            <Link color="inherit" component={RouterLink} className={classes.navLink} to="/editor/">Editor</Link>
+                            <Link color="inherit" component={RouterLink} className={classes.navLink} to="/users/">Users</Link>
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
 
-            <Route path="/" exact component={Index} />
-            <Route path="/editor/" component={EditorComponent} />
-            <Route path="/users/" component={Users} />
-        </Router>
+                <Container maxWidth={false}>
+                    <Route path="/" exact component={Index} />
+                    <Route path="/editor/" component={EditorComponent} />
+                    <Route path="/users/" component={Users} />
+                </Container>
+            </Router>
+        </>
     );
 }
